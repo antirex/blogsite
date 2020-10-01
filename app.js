48010b97948ccedd17af0dfe53e03fde63190358
includeavaneesh <54032418+includeavaneesh@users.noreply.github.com>
2020-10-01 11:39:23 +0530

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const aboutContent =
   "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent =
   "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
-
+col.style.color="red";
 const app = express();
 
 app.set("view engine", "ejs");
@@ -65,7 +65,7 @@ app.post("/compose", function (req, res) {
   postsArray.push(post);
   res.redirect("/");
 });
-
+<button onclick="display()">Display</button>
 app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
